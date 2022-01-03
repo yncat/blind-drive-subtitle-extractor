cba44f79df6328a5ca62a1ae616186a5a78d1158
Yukio Nozawa <personal@nyanchangames.com>
2022-01-03 20:30:10 +0900

    Fix format

--- extract-ui.py
+++ extract-ui.py
@@ -2,24 +2,48 @@ import argparse
 import sys
 import pandas as pd
 
+
 def checkargs(args):
     if args.list_languages:
         return True
     # end list languages
-    missing = ["--%s" % (x) for x in ["input", "output", "language"] if not getattr(args, x)]
-    if len(missing)>0:
-        sys.stderr.write("You must specify the following options. Run extract.py --help for details:\n%s\n" % "\n".join(missing))
+    missing = [
+        "--%s" %
+        (x) for x in [
+            "input",
+            "output",
+            "language"] if not getattr(
+            args,
+            x)]
+    if len(missing) > 0:
+        sys.stderr.write(
+            "You must specify the following options. Run extract.py --help for details:\n%s\n" %
+            "\n".join(missing))
         return False
     # end error
     return True
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", help="Path to the input file", required=True)
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="Path to the input file",
+        required=True)
     parser.add_argument("-o", "--output", help="Path to the output file")
-    parser.add_argument("-l", "--language", help="language of the subtitle to extract")
-    parser.add_argument("--include-original", help="include original english text as well", action="store_true")
-    parser.add_argument("--list-languages", help="show available languages and exit", action="store_true")
+    parser.add_argument(
+        "-l",
+        "--language",
+        help="language of the subtitle to extract")
+    parser.add_argument(
+        "--include-original",
+        help="include original english text as well",
+        action="store_true")
+    parser.add_argument(
+        "--list-languages",
+        help="show available languages and exit",
+        action="store_true")
     args = parser.parse_args()
     if not checkargs(args):
         return 1
@@ -27,11 +51,11 @@ def main():
     df = pd.read_csv(args.input)
     if args.list_languages:
         exc = "key,twine,en,en-styled,comment".split(",")
-        langs = [x for x in list(df.columns) if (not x in exc)]
+        langs = [x for x in list(df.columns) if (x not in exc)]
         print("The following languages are available:\n%s" % "\n".join(langs))
         return 0
     # end list languages
-    if not args.language in df.columns:
+    if args.language not in df.columns:
         sys.stderr.write("Language %s is not found.\n" % args.language)
         return 1
     # end language not found
@@ -41,5 +65,6 @@ def main():
     extracted = df.loc[:, flt]
     extracted.to_csv(args.output, index=False)
     return 0
+
 
 sys.exit(main())
